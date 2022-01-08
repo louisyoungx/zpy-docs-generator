c650e21c996ba3bc0b72992c2478f57f61cc7d55
louisyoungx <1462648167@qq.com>
2022-01-09 05:12:22 +0800

    perf(requests): optimize requests lib

--- test/get_maps.py
+++ test/get_maps.py
@@ -4,4 +4,5 @@ if ".." not in sys.path: sys.path.insert(0,"..")
 from lib import lib
 print(lib.pyLibs)
 lib.load('time', 'zpy')
+lib.load('requests', 'zpy')
 print(lib.zpyMap)
--- test/lib_conflict.py
+++ test/lib_conflict.py
@@ -5,9 +5,11 @@ from lib import Lib
 
 from lib.__builtin__ import BUILT_IN
 from lib.standard import STANDARD
+from lib.requests import REQUESTS
 
-lib = Lib(BUILT_IN, 'error')
+lib = Lib(BUILT_IN, 'warn')
 
-lib.use(STANDARD)
+lib.use(STANDARD).use(REQUESTS)
 
 lib.load('time', 'zpy')
+lib.load('requests', 'zpy')
